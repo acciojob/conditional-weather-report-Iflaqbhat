8056ba403a695c4381e3019dfeb2350e1325b61d
iflaqbhat <ifuubhat72@gmail.com>
2024-12-18 22:06:13 +0530

    ,,,

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const WeatherDisplay = (props) => {
   const tempStyle =
-    props.temperature >= 20 ? { color: "red" } : { color: "blue" };
+    props.temperature > 20 ? { color: "red" } : { color: "blue" };
+
   return (
     <div>
-      <p style={tempStyle}>{props.temperature}</p>
-      <p>{props.conditions}</p>
+      <p>
+        Temperature: <span style={tempStyle}>{props.temperature}</span>
+      </p>
+      <p>Conditions: {props.conditions}</p>
     </div>
   );
 };
